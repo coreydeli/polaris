c333d1397c4dad6cf3060345ddd0900f89451f9b
papi <papi@papi-ux.com>
2026-09-12 16:07:55 -0400

    Include GLES2 loaders in every graphics runtime

--- containers/multiseat/resolve-packages.sh
+++ containers/multiseat/resolve-packages.sh
@@ -25,6 +25,7 @@ case "$profile" in
   heroic) runtime="$runtime /launchers/heroic.deb mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386" ;;
   lutris) runtime="$runtime lutris wine winetricks mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386" ;;
 esac
+runtime="$runtime libgles2"
 if [ "$profile" != gamescope ]; then
   runtime="$runtime libegl1:i386 libgles2:i386 libgl1:i386 libwayland-server0:i386"
 fi
